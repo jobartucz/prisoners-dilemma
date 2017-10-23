182b8554c8d3e48e82c9afe7d85e2c7ff390d882
jb <john@bartucz.com>
2017-10-23 07:34:58 -0500

    updated Mr. B to fix comment error
    updated Mr. B team0 to fix comment error. Added some comments to the tournament file.

--- team0.py
+++ team0.py
@@ -7,12 +7,12 @@
 ####
 
 team_name = 'Mr. B' # Only 10 chars displayed.
-strategy_name = 'Rubber Chicken'
+strategy_name = 'Random'
 strategy_description = 'Can\'t tell you yet'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
-    my_score, their_score are ints.
+    my_score, their_score are ints.'''
     
     import random
     if (random.randint(0,100) < 50):
