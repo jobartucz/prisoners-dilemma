2d062563b820d82d092e88688f3d88801453a37f
FlaminFirebird <32515344+FlaminFirebird@users.noreply.github.com>
2017-10-24 08:13:07 -0500

    HEAd

--- prisoners_dilemma.py
+++ prisoners_dilemma.py
@@ -44,7 +44,6 @@ collude = example0
 modules = [example0, example1, example2, example3, example4, example5, example6, example7,
 team0, team1, team2, team3, team4, team5, team6, team7, team8, team9, team10, 
 team11, team12, team13, team14, team15]
-<<<<<<< HEAD
 
 modules = [example0, example1, example2, example3, example4, example5, example6, example7, team13]
 
