6fa6587e91022c89486bc87211bab2b18623f713
Mustangs6419 <32515317+Mustangs6419@users.noreply.github.com>
2017-10-24 08:51:13 -0500

    Trying to improve my score part two

--- team9.py
+++ team9.py
@@ -31,7 +31,7 @@ def move(my_history, their_history, my_score, their_score):
         return "b"
     else:
         return "c"
-    if their_history[-4]:
+    if "b" in their_history[-4]:
         return "b"
     else:
         return "c"
