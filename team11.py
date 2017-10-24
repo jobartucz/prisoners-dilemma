36d8548be4ea38886d1a9517e81aa21ea09ef21f
mk1105 <32515132+mk1105@users.noreply.github.com>
2017-10-24 09:07:01 -0500

    tit for tat

--- team11.py
+++ team11.py
@@ -25,10 +25,10 @@ def move(my_history, their_history, my_score, their_score):
     
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
-    if 'c' in their_history:
-        return 'c'
-    else:
+    if 'b' in their_history:
         return 'b'
+    else:
+        return 'c'
     
 
     
