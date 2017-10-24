540e6792cc12677a5bc8a610b6d46f95cd49eea4
Noah S <7339901@RPS.net>
2017-10-24 08:03:33 -0500

    change my lesson

--- team1.py
+++ team1.py
@@ -12,7 +12,7 @@ strategy_description = 'The program makes the same move as the opponents previou
     
 def move(my_history, their_history, my_score, their_score):
     
-    if their_history[:-1]=='b':
+    if len (their_history)=='b':
         return 'c'
     else:
         return 'b'
