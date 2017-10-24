ba943e6b9ba9a839814207ad635870643335ea30
fibgibb <32515324+fibgibb@users.noreply.github.com>
2017-10-24 09:04:31 -0500

    terrible formula
    Supposed to be collude until betray

--- team8.py
+++ team8.py
@@ -7,17 +7,17 @@
 ####
 
 team_name = 'Finlay' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
-    
-def move(my_history, their_history, my_score, their_score):
+strategy_name = 'rad strat'
+strategy_description = 'Always betray, unless opponent starts colluding'
+
+def move(myHist, opHist, myHits, opHits):
     ''' Arguments accepted: my_history, their_history are strings.
     my_score, their_score are ints.
     
     Make my move.
     Returns 'c' or 'b'. 
     '''
-
+    
     # my_history: a string with one letter (c or b) per round that has been played with this opponent.
     # their_history: a string of the same length as history, possibly empty. 
     # The first round between these two players is my_history[0] and their_history[0].
@@ -25,8 +25,15 @@ def move(my_history, their_history, my_score, their_score):
     
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
-    
-    return 'c'
+    ans = 'c'
+    if(len(myHist) > 0):
+        bool = True
+        for i in myHist:
+            if(i == 'b'):
+                bool = False
+        if(bool):
+            ans = 'c'
+    return ans
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
