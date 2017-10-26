8f59701e2f9dea6ff752f4ea396073f0aff39081
MrMeSMG <9591701@isd535.org>
2017-10-26 09:10:57 -0500

    Oops
    Submitted this 3 days ago, but accidentally did it in a branch, so it didn't partake in the tourney. Oops.

--- team10.py
+++ team10.py
@@ -7,28 +7,22 @@
 ####
 
 team_name = 'Scott' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+strategy_name = 'The Strategy'
+strategy_description = 'Colludes unless enemy betrayed last 2 turns'
     
 def move(my_history, their_history, my_score, their_score):
-    ''' Arguments accepted: my_history, their_history are strings.
-    my_score, their_score are ints.
-    
-    Make my move.
-    Returns 'c' or 'b'. 
-    '''
-
-    # my_history: a string with one letter (c or b) per round that has been played with this opponent.
-    # their_history: a string of the same length as history, possibly empty. 
-    # The first round between these two players is my_history[0] and their_history[0].
-    # The most recent round is my_history[-1] and their_history[-1].
-    
-    # Analyze my_history and their_history and/or my_score and their_score.
-    # Decide whether to return 'c' or 'b'.
-    
-    return 'c'
-
-    
+    x = len(my_history)
+    y = len(their_history)
+    if x == 0:
+        return 'c'
+    elif x == 1:
+        if their_history[y - 1] == 'b':
+            return 'b'
+    elif their_history[y - 1] == 'b':
+        if their_history[y - 2] == 'b':
+            return 'b'
+    else:
+        return 'c'
 def test_move(my_history, their_history, my_score, their_score, result):
     '''calls move(my_history, their_history, my_score, their_score)
     from this module. Prints error if return value != result.
@@ -52,11 +46,11 @@ if __name__ == '__main__':
               their_history='', 
               my_score=0,
               their_score=0,
-              result='b'):
+              result='c'):
          print ('Test passed')
      # Test 2: Continue betraying if they collude despite being betrayed.
-    test_move(my_history='bbb',
-              their_history='ccc', 
+    test_move(my_history='ccc',
+              their_history='cbb', 
               # Note the scores are for testing move().
               # The history and scores don't need to match unless
               # that is relevant to the test of move(). Here,
